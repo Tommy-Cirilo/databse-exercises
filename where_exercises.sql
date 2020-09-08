84db2f594f00a405030cc22e96f8cc5d124c68f7
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-09-08 13:59:59 -0500

    completed the where exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,12 +1,52 @@
 use employees;
 
-# select * from employees where hire_date = '1985-01-01';
-
-select first_name
+select *
 from employees
 where first_name in ('Irena' , 'Vidya' , 'Maya');
 
-select last_name
+
+
+# alternate
+/*select *
 from employees
-where last_name like ('%E%');
+where (first_name = 'Irena'
+    or first_name = 'Vidya'
+    or first_name = 'Maya')
+    and gender = 'm';*/
+
+select *
+from employees
+where last_name like ('E%') or last_name like ('%e');
+
+select *
+from employees
+where hire_date like ('199%');
+
+# alternate
+/*select *
+from employees
+where hire_date like ('199%') and birth_date like ('%12-25');
+*/
+
+
+# alternate
+/*select *
+from employees
+where hire_date*/
+
+
+select *
+from employees
+where birth_date like ('%12-25');
+
+select *
+from employees
+where last_name like ('%q%');
+
+# alternate
+/*select *
+from employees
+where last_name like ('%q%') and last_name not like '%qu%';
+*/
+
 
