953a71db2f26415bd3d5d42f9c77968e23edb42d
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-09-08 12:29:22 -0500

    saving database work

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,7 +1,12 @@
 use employees;
 
-select * from employees where hire_date = '1985-01-01';
+# select * from employees where hire_date = '1985-01-01';
 
 select first_name
 from employees
-where first_name like '%sus%';
+where first_name in ('Irena' , 'Vidya' , 'Maya');
+
+select last_name
+from employees
+where last_name like ('%E%');
+
