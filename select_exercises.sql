3dc9084cf1822ee4e5119eb1536d681a3e56229a
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-09-09 22:00:30 -0500

    updating the select exercise

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,2 +1,28 @@
-use codeup_test_db;
+USE codeup_test_db;
 
+select name
+from albums
+where artist = 'Pink Floyd';
+
+select release_date
+from albums
+where name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+select genre
+from albums
+where name = 'Nevermind';
+
+select *
+from albums
+where release_date between 1990 and 1999;
+
+select *
+from albums
+where sales < 20;
+
+select *
+from albums;
+
+select *
+from albums
+where genre = 'Rock';
