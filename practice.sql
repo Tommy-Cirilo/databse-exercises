e1bb1e277a26f29f59a5eca968ad0a602f312b56
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-09-13 00:23:36 -0500

    working on buildng the starwars database

--- practice.sql
+++ practice.sql
@@ -12,6 +12,41 @@ create table sithLords
 create table sithRank
 (
     id              int unsigned not null auto_increment,
-    rankHierarchy            varchar(255) not null,
+    rankHierarchy   varchar(255) not null,
     rankDescription varchar(255) not null
+);
+
+create table jediMasters
+(
+    id   int unsigned not null auto_increment,
+    name varchar(255) not null,
+    primary key (id)
+);
+
+create table jediRank
+(
+    id              int unsigned not null auto_increment,
+    rankHierarchy   varchar(255) not null,
+    rankDescription varchar(255) not null
+);
+create table meleeWeapons
+(
+    id int unsigned not null auto_increment
+);
+
+create table rangedWeapons
+(
+    id int unsigned not null auto_increment
+);
+
+create table forcePowers
+(
+    id int unsigned not null auto_increment,
+    name varchar(100) not null ,
+    faction varchar(10)
+);
+
+create table planets
+(
+    id int unsigned not null auto_increment
 );
