bc05cdfee8f8f959a44dd085c29af1dfbd7d44f3
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-09-12 23:42:12 -0500

    creating a database and practicing the mysql fundamentals

--- practice.sql
+++ practice.sql
@@ -1,2 +1,17 @@
-use employees;
+use sithEmpire;
 
+create database sithEmpire;
+
+create table sithLords
+(
+    id   int unsigned not null auto_increment,
+    name varchar(255) not null,
+    primary key (id)
+);
+
+create table sithRank
+(
+    id              int unsigned not null auto_increment,
+    rankHierarchy            varchar(255) not null,
+    rankDescription varchar(255) not null
+);
